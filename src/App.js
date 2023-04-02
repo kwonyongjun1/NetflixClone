923168b0712952a004b65ed231575bd75817cd77
YongJun Kwon <dydwns18011@gmail.com>
2023-04-02 14:44:55 +0900

    feat: row 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import './App.css';
+import requests from './api/requests';
 import Banner from './components/Banner';
 import Nav from './components/Nav';
-
+import Row from './components/Row';
 function App() {
   return (
-    <div>
+    <div style={{backgroundColor : '#000000'}}>
       <Nav/>
       <Banner/>
+      <Row
+        title="NETFLIX_ORIGINALS"
+        id="NO"
+        fetchURL={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+
+      <Row
+        title="Trending Now"
+        id="TN"
+        fetchURL={requests.fetchTrending}
+        isLargeRow
+      />
+
+      <Row    
+        title="Top Rated"
+        id="TR"
+        fetchURL={requests.fetchTopRated}
+        isLargeRow
+      />
+      
+      <Row    
+        title="Action Movies"
+        id="AM"
+        fetchURL={requests.fetchActionMovies}
+        isLargeRow
+      />
+
+      <Row    
+        title="Comedy Movies"
+        id="CM"
+        fetchURL={requests.fetchComedyMovies}
+        isLargeRow
+      />
     </div>
   );
 }
