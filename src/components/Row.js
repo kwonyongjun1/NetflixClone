82f6dfd8184196e06f1cd34f0bd63ce92b08342f
YongJun Kwon <dydwns18011@gmail.com>
2023-04-03 22:59:02 +0900

    feat: 상세보기 Modal 컴포넌트 생성

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,9 +1,12 @@
 import axios from '../api/axios';
 import React,{useEffect, useState} from 'react';
 import './Row.css'
+import MovieModal from './MovieModal';
 
 export default function Row({title,id,fetchURL,isLargeRow}) {
   const [movies, setMovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelected] = useState({});
 
   useEffect(()=>{
     fetchMovieData();
@@ -14,6 +17,10 @@ export default function Row({title,id,fetchURL,isLargeRow}) {
     setMovies(request.data.results);
   }
 
+  const handleClick = (movie) =>{
+    setModalOpen(true);
+    setMovieSelected(movie);
+  }
   return (
     <section>
       <h2 className='row'>{title}</h2>
@@ -36,6 +43,7 @@ export default function Row({title,id,fetchURL,isLargeRow}) {
                 isLargeRow ? movie.poster_path : movie.backdrop_path
               } `}
               alt = {movie.name}
+              onClick={()=>{ handleClick(movie)}}
             />
           ))}
         </div>
@@ -49,6 +57,12 @@ export default function Row({title,id,fetchURL,isLargeRow}) {
             </span>
         </div>
       </div>
+          {
+            modalOpen && (
+            <MovieModal {...movieSelected} setModalOpen = {setModalOpen}/>
+            )
+          }
+
     </section>
   )
 }
