1c8f636ce6502c41e993ee153b7371ca4af51eca
YongJun Kwon <dydwns18011@gmail.com>
2023-04-06 22:26:51 +0900

    feat: 영화 검색 기능 UI 추가

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom';
 import axios from '../../api/axios';
+import './SearchPage.css'
 export default function SearchPage() {
 
     const [searchResults, setSearchResults] = useState([]);
@@ -25,12 +26,41 @@ export default function SearchPage() {
             )
             setSearchResults(request.data.results);
         } catch(error){
-
+            console.log("error", error);       
         }
     }
 
+    const renderSearchResult = () =>{
+        return searchResults.length > 0 ? (
+            <section className='search-container'>
+                {searchResults.map((movie,index)=>{
+                    if(movie.backdrop_path !== null && movie.media_type !== "person"){
+                        const movieImageUrl = 
+                        "https://image.tmdb.org/t/p/w500" + movie.backdrop_path
+                        return(
+                            <div className='movie'>
+                                <div className='movie__column-poster'>
+                                    <img
+                                    src={movieImageUrl} alt="movie"
+                                    className='movie__poster'                                    
+                                    />
+                                </div>
+                            </div>
+                        )
+                    }
+                })}
+            </section>
+        ):(
+            <section className='no-results'>
+                <div className='no-results__text'>
+                    <p>
+                        찾고자하는 검색어 "{searchTerm}" 찾고자하는 영화가 없습니다.
+                    </p>
+                </div>
+            </section>
+        )
+    }
+
     
-  return (
-    <div></div>
-  )
+  return renderSearchResult();
 }
