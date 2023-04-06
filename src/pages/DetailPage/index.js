380e1745cf6a93d68b5cd767216a2bd8b75874b5
YongJun Kwon <dydwns18011@gmail.com>
2023-04-06 23:25:14 +0900

    feat: 검색한 영화 상세페이지 추가

--- src/pages/DetailPage/index.js
+++ src/pages/DetailPage/index.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useEffect, uesState } from 'react'
+import { useParams} from 'react-router-dom'
+import axios from '../../api/axios'
 
 export default function DetailPage() {
+  const {movieId} = useParams();
+  const [movie, setMovie] = uesState({});
+
+  useEffect(()=>{
+    async function fetchData(){
+      const request = await axios.get(
+        `movie/${movieId}`
+      )
+      setMovie(request.data);
+    }
+    fetchData();
+  }, [movieId]);
+
+  if(!movie)  return(  <div>...loading</div>  )
+  
   return (
-    <div>index</div>
+    <section>
+      <img
+      className='modal__poster-img'
+      src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+      alt="postser"
+      />
+    </section>
   )
+  
+  
 }
